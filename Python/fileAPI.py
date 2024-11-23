f21566244e19a4c848488a36878090f090e95dce
Alok Verma <as1492@srmist.edu.in>
2024-11-23 19:01:49 +0530

    Port

--- Python/fileAPI.py
+++ Python/fileAPI.py
@@ -150,4 +150,5 @@ def rename_file():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
